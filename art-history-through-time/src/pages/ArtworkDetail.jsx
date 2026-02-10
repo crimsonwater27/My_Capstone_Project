03615b637ca49bcb8d377397e5f932180e6f1315
Cole <utsukuskiotaku17@gmail.com>
2026-02-10 19:01:34 +0200

    implement artwork detail page

--- art-history-through-time/src/pages/ArtworkDetail.jsx
+++ art-history-through-time/src/pages/ArtworkDetail.jsx
@@ -1,7 +1,23 @@
-export default function Favorites() {
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getArtwork } from "../services/museumApi";
+
+export default function ArtworkDetail() {
+  const { id } = useParams();
+  const [art, setArt] = useState(null);
+
+  useEffect(() => {
+    getArtwork(id).then(setArt);
+  }, [id]);
+
+  if (!art) return <p>Loading...</p>;
+
   return (
-    <div className="text-white p-8">
-      Artwork Detail Page
+    <div>
+      <h2>{art.title}</h2>
+      <img src={art.primaryImage} width="300" />
+      <p>{art.artistDisplayName}</p>
+      <p>{art.objectDate}</p>
     </div>
   );
 }
