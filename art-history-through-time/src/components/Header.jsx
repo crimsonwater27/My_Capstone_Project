b8924a843bacf55cdd0dcd84d99fbcd060992549
Cole <utsukuskiotaku17@gmail.com>
2026-02-10 20:02:41 +0200

    add Header and Footer components

---
+++ art-history-through-time/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>History Through Time</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/timeline">Timeline</a>
+        <a href="/favorites">Favorites</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
