7ddd7024e9b625f884933e936e302e32d2555963
Cole <utsukuskiotaku17@gmail.com>
2026-02-10 20:12:30 +0200

    add Filters component for era filtering

--- art-history-through-time/src/components/Filters.jsx
+++ art-history-through-time/src/components/Filters.jsx
@@ -5,9 +5,10 @@ const Filters = ({ onFilterChange }) => {
     <div>
       <select onChange={(e) => onFilterChange(e.target.value)}>
         <option value="">All Eras</option>
-        <option value="ancient">Ancient</option>
+        <option value="baroque">Baroque</option>
         <option value="renaissance">Renaissance</option>
         <option value="modern">Modern</option>
+        <option value="romanticism">Romanticism</option>
       </select>
     </div>
   );
