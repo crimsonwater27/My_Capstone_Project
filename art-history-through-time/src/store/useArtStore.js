b0905e9a2328808752c8738b410170d60a246dd6
Cole <utsukuskiotaku17@gmail.com>
2026-02-10 19:31:09 +0200

    add timeline filtering logic to store

--- art-history-through-time/src/store/useArtStore.js
+++ art-history-through-time/src/store/useArtStore.js
@@ -1,21 +1,41 @@
 import { create } from "zustand";
 import { searchArtworks, getArtwork } from "../services/museumApi";
+import { parseYear } from "../utils/parseYear";
 
-export const useArtStore = create((set) => ({
+export const useArtStore = create((set, get) => ({
   artworks: [],
   favorites: [],
   selectedArtwork: null,
   yearRange: [1400, 1800],
 
-  setYearRange: (range) => set({ yearRange: range }),
-
-  fetchArtworks: async () => {
-    const ids = await searchArtworks("art");
-    const data = await Promise.all(ids.map(id => getArtwork(id)));
-    set({ artworks: data });
+  setYearRange: (range) => {
+    set({ yearRange: range });
+    getArtwork().filteredArtworks();
   },
 
-  selectArtwork: (art) => set({ selectedArtwork: art }),
+
+  fetchArtworks: async () => {
+    const ids = await searchArtworks("painting");
+    const data = await Promise.all(ids.map(id => getArtwork(id)));
+    set({ 
+      artworks: data,
+      filteredArtworks: data,
+    });
+  },
+     
+filteredArtworks: () => {
+  const { artworks, yearRange } = get();
+
+  const filtered = artworks.filter((art) => {
+    const year = parseYear(art.ObjectDate);
+    if (!year) return false;
+
+    return year >= yearRange[0] && year <= yearRange[1];
+  });
+
+  set({ filteredArtworks: filtered });
+
+},
 
   addFavorite: (art) =>
     set((state) => ({
